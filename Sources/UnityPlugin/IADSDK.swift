c8ab585685c019fecf74f1a90b7e3c571ea85039
Steve <steveshelaiev99@gmail.com>
2024-02-13 18:12:00 +0200

    attributionToken fetching

--- Sources/UnityPlugin/IADSDK.swift
+++ Sources/UnityPlugin/IADSDK.swift
@@ -10,75 +10,15 @@ import AdServices
 
 
 public struct IADSDK {
-    
+
     @available(iOS 14.3, *)
-    public static func getCampaignId(with appleID: String, completion: @escaping (Result<Int, Error>) -> Void) {
+    public func getAttributionToken() -> String {
         do {
             let attributionToken = try AAAttribution.attributionToken()
-            
-            let data = attributionToken.data(using: .utf8)
-            
-            guard let baseUrl = URL(string: appleID) else {
-                return
-            }
-            
-            let request = NSMutableURLRequest(url: baseUrl)
-            request.httpMethod = "POST"
-            request.setValue("text/plain", forHTTPHeaderField: "Content-Type")
-            request.httpBody = data
-            
-            let task = URLSession.shared.dataTask(with: request as URLRequest) { (data, response, error) in
-               
-                guard let httpResponse = response as? HTTPURLResponse else {
-                    getCampaignId(with: appleID, completion: completion)
-                    return
-                }
-                
-                switch httpResponse.statusCode {
-                case 200:
-                    guard let data = data else {
-                        getCampaignId(with: appleID, completion: completion)
-                        return
-                    }
-                    
-                    do {
-                        guard let json = try JSONSerialization.jsonObject(
-                            with: data,
-                            options: .fragmentsAllowed
-                        ) as? [String: Any] else {
-                            completion(.failure(IAdError.noCampaignId))
-                            return
-                        }
-                        
-                        if let campaignId = json["campaignId"] as? Int {
-                            completion(.success(campaignId))
-                        } else {
-                            completion(.failure(IAdError.noCampaignId))
-                        }
-                        
-                    } catch {
-                        completion(.failure(IAdError.failedToSerializeJson))
-                    }
-                    break
-                case 404:
-                    getCampaignId(with: appleID, completion: completion)
-                    break
-                default:
-                    completion(.failure(IAdError.iAdServerError))
-                    break
-                }
-            }
-            task.resume()
+            return attributionToken
         } catch {
-            completion(.failure(IAdError.failedToGetAttributionToken))
+            return ""
         }
     }
     
-}
-
-enum IAdError: Error {
-    case iAdServerError
-    case failedToGetAttributionToken
-    case noCampaignId
-    case failedToSerializeJson
 }
